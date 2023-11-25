bc4e11cdbd49cdef4c3485dc77c1ea1e1279cff8
Cossio <estebancogo47@gmail.com>
2023-11-24 19:29:21 -0500

    Setting the base for development ⭐

--- cofilmo-front/src/App.vue
+++ cofilmo-front/src/App.vue
@@ -1,5 +1,22 @@
+<script setup lang="ts">
+import Card from "./components/Card.vue";
+import { ref } from "vue";
+
+type Option = {
+  label: string;
+};
+
+const options = ref<Option[]>([
+  { label: "Create" },
+  { label: "Watch" },
+  { label: "Random" },
+]);
+</script>
+
 <template>
-  <div class="bg-slate-900">
-    <h1>HOLA</h1>
-  </div>
+  <main
+    class="items-center justify-center h-screen flex grid-cols-2 bg-green-950"
+  >
+    <Card v-for="option in options" :option="option.label" class="m-8" />
+  </main>
 </template>
---
+++ cofilmo-front/src/components/Card.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="card bg-emerald-100 rounded-full shadow-md flex justify-center items-center">
+    <h1 class="text-2xl text-green-800 font-semibold">
+      {{ option }}
+    </h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps(["option"])
+</script>
