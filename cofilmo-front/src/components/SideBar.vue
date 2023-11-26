c5ef094e1114752532cb037540493a5b113966c7
Cossio <estebancogo47@gmail.com>
2023-11-26 08:52:32 -0500

    Refactor sidebar component using router

--- cofilmo-front/src/components/SideBar.vue
+++ cofilmo-front/src/components/SideBar.vue
@@ -17,8 +17,12 @@
         <section
           class="text-xl font-medium flex flex-col text-emerald-700 dark:text-emerald-50"
         >
-          <span class="text font-semibold" :class="{ hidden: sidebarClosed }">Cofilmo</span>
-          <span class="text font-medium" :class="{ hidden: sidebarClosed }">Movie Tracker</span>
+          <span class="text font-semibold" :class="{ hidden: sidebarClosed }"
+            >Cofilmo</span
+          >
+          <span class="text font-medium" :class="{ hidden: sidebarClosed }"
+            >Movie Tracker</span
+          >
         </section>
       </div>
       <i
@@ -27,21 +31,29 @@
       ></i>
     </header>
 
-    <section class="h-[100%-50px] flex flex-col justify-between whitespace-nowrap py-24">
+    <section
+      class="h-[100%-50px] flex flex-col justify-between whitespace-nowrap py-24"
+    >
       <div>
         <ul>
           <li
             class="h-10 m-5 list-none shadow-md shadow-emerald-400 bg-emerald-300 dark:bg-emerald-950 dark:shadow-zinc-900"
           >
-            <a
-              href="#"
-              class="flex items-center justify-start h-full p-2 rounded-md text-xl text-emerald-800 hover:bg-emerald-800 hover:text-white dark:hover:bg-emerald-300 dark:hover:text-emerald-900 dark:text-emerald-50 transition duration-300 font-medium font-dosis"
-            >
-              <i class="bx bx-home-alt min-w-[2.2rem] text-3xl transition"></i>
-              <span class="text ml-8 text-xl " :class="{ hidden: sidebarClosed }"
-                >Home</span
+            <router-link to="/">
+              <a
+                href="#"
+                class="flex items-center justify-start h-full p-2 rounded-md text-xl text-emerald-800 hover:bg-emerald-800 hover:text-white dark:hover:bg-emerald-300 dark:hover:text-emerald-900 dark:text-emerald-50 transition duration-300 font-medium font-dosis"
               >
-            </a>
+                <i
+                  class="bx bx-home-alt min-w-[2.2rem] text-3xl transition"
+                ></i>
+                <span
+                  class="text ml-8 text-xl"
+                  :class="{ hidden: sidebarClosed }"
+                  >Home</span
+                >
+              </a>
+            </router-link>
           </li>
           <li
             class="h-10 m-5 list-none shadow-md shadow-emerald-400 bg-emerald-300 dark:bg-emerald-950 dark:shadow-zinc-900"
@@ -53,7 +65,7 @@
               <i
                 class="bx bx-plus-circle min-w-[2.2rem] text-3xl transition"
               ></i>
-              <span class="text ml-8 text-xl " :class="{ hidden: sidebarClosed }"
+              <span class="text ml-8 text-xl" :class="{ hidden: sidebarClosed }"
                 >Add</span
               >
             </a>
@@ -62,17 +74,21 @@
           <li
             class="h-10 m-5 list-none shadow-md shadow-emerald-400 bg-emerald-300 dark:bg-emerald-950 dark:shadow-zinc-900"
           >
-            <a
-              href="#"
-              class="flex items-center justify-start h-full p-2 rounded-md text-xl text-emerald-800 hover:bg-emerald-800 hover:text-white dark:hover:bg-emerald-300 dark:hover:text-emerald-900 dark:text-emerald-50 transition duration-300 font-medium font-dosis"
-            >
-              <i
-                class="bx bx-movie-play min-w-[2.2rem] text-3xl transition"
-              ></i>
-              <span class="text ml-8 text-xl " :class="{ hidden: sidebarClosed }"
-                >Watch</span
+            <router-link to="/watch">
+              <a
+                href="#"
+                class="flex items-center justify-start h-full p-2 rounded-md text-xl text-emerald-800 hover:bg-emerald-800 hover:text-white dark:hover:bg-emerald-300 dark:hover:text-emerald-900 dark:text-emerald-50 transition duration-300 font-medium font-dosis"
               >
-            </a>
+                <i
+                  class="bx bx-movie-play min-w-[2.2rem] text-3xl transition"
+                ></i>
+                <span
+                  class="text ml-8 text-xl"
+                  :class="{ hidden: sidebarClosed }"
+                  >Watch</span
+                >
+              </a>
+            </router-link>
           </li>
 
           <li
@@ -83,7 +99,7 @@
               class="flex items-center justify-start h-full p-2 rounded-md text-xl text-emerald-800 hover:bg-emerald-800 hover:text-white dark:hover:bg-emerald-300 dark:hover:text-emerald-900 dark:text-emerald-50 transition duration-300 font-medium font-dosis"
             >
               <i class="bx bx-dice-3 min-w-[2.2rem] text-3xl transition"></i>
-              <span class="text ml-8 text-xl " :class="{ hidden: sidebarClosed }"
+              <span class="text ml-8 text-xl" :class="{ hidden: sidebarClosed }"
                 >Random</span
               >
             </a>
@@ -94,7 +110,12 @@
       <div class="relative py-14 toggle">
         <div class="flex justify-center items-center p-4 absolute">
           <label class="switch bg-emerald-300">
-            <input type="checkbox" id="darkmode" @change="toggleDarkMode" :checked="darkMode.valueOf" />
+            <input
+              type="checkbox"
+              id="darkmode"
+              @change="toggleDarkMode"
+              :checked="darkMode.valueOf"
+            />
             <span class="slider"></span>
           </label>
         </div>
@@ -105,7 +126,7 @@
 
 <script setup lang="ts">
 import { ref } from "vue";
-import { darkMode, toggleDarkMode } from '../utils/DarkMode.ts';
+import { darkMode, toggleDarkMode } from "../utils/DarkMode.ts";
 
 const sidebarClosed = ref(true);
 
