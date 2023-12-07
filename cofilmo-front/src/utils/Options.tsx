c259c05f8268c4320f735617126524a057e2b191
Cossio <estebancogo47@gmail.com>
2023-12-07 11:03:55 -0500

    Add style folder

--- cofilmo-front/src/utils/Options.tsx
+++
@@ -1,6 +0,0 @@
-export const options = [
-  { to: "/", icon: "bx bx-home-alt", text: "Home" },
-  { to: "/add", icon: "bx bx-plus-circle", text: "Add" },
-  { to: "/watch", icon: "bx bx-movie-play", text: "Watch" },
-  { to: "/random", icon: "bx bx-dice-3", text: "Random" },
-];
