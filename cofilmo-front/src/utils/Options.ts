4b810ab2dd1b1ff93e78a5e6611c26f65541b3bd
Cossio <estebancogo47@gmail.com>
2023-12-28 09:12:02 -0500

    Adding id for each option 👁️

--- cofilmo-front/src/utils/Options.ts
+++ cofilmo-front/src/utils/Options.ts
@@ -1,6 +1,6 @@
 export const options = [
-  { to: "/", icon: "bx bx-home-alt", text: "Home" },
-  { to: "/add", icon: "bx bx-plus-circle", text: "Add" },
-  { to: "/watch", icon: "bx bx-movie-play", text: "Watch" },
-  { to: "/random", icon: "bx bx-dice-3", text: "Random" },
+  { id: 1, to: "/", icon: "bx bx-home-alt", text: "Home" },
+  { id: 2, to: "/add", icon: "bx bx-plus-circle", text: "Add" },
+  { id: 3, to: "/watch", icon: "bx bx-movie-play", text: "Watch" },
+  { id: 4, to: "/random", icon: "bx bx-dice-3", text: "Random" },
 ];
