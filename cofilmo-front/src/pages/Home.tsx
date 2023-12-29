12a6fc193e0ea6655d948ea5865cdece70d07f89
Cossio <estebancogo47@gmail.com>
2023-12-29 11:27:24 -0500

    Editing home page 🧰

--- cofilmo-front/src/pages/Home.tsx
+++ cofilmo-front/src/pages/Home.tsx
@@ -1,12 +1,21 @@
+import EternalSunshine from "../assets/EternalSunshine.webp";
+
 export const Home = () => {
   return (
     <div className="flex flex-col justify-center items-center">
-      <h1 className="text-4xl font-bold text-emerald-900 dark:text-emerald-50">
-        Home Page
+      <div className="relative w-[28rem] h-[16rem] overflow-hidden rounded-md">
+        <img
+          src={EternalSunshine}
+          alt="Eternal Sunshine Image"
+          className="w-full h-full object-cover rounded-md"
+        />
+      </div>
+      <h1 className="text-4xl font-bold text-emerald-900 dark:text-emerald-50 mt-4">
+        Your movie library
       </h1>
       <p className="text-xl text-emerald-900 dark:text-emerald-50">
-        This is the home page
+        To remember the emotions that you felt watching them
       </p>
     </div>
   );
-}
+};
